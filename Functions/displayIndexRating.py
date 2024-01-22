7f3efc8e9b74cf601297ba6eebec92ba89b1e4ed
Garrett Ellis <154717520+garrettbovo@users.noreply.github.com>
2024-01-21 19:35:58 -0500

    Delete Functions directory

--- Functions/displayIndexRating.py
+++
@@ -1,7 +0,0 @@
-def displayIndexRating(restaurant, index):
-    """This function will the rating at the specified index within the specified restaurant's list.  This function also takes in one mutable, argument which is stored into restaurant, and one immutable argument which is stored into index.""" 
-
-    #printing rating at specified restaurant and index to user
-    print("User: " + restaurant.getRatings(index).getUser())
-    print("Rating Value: " + str(restaurant.getRatings(index).getRatingValue()))
-    print("Comment: " + restaurant.getRatings(index).getComment() + "\n")
